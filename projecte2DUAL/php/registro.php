5435976ea7a2392bb790f76d9b19adf9c1d79816
joelgarciahurtado <joelgarciah@inslapineda.cat>
2020-10-26 14:18:41 +0100

    actualicación

--- projecte2DUAL/php/registro.php
+++ projecte2DUAL/php/registro.php
@@ -9,7 +9,6 @@
 
 $_POST['contrasenya']="'".md5($_POST['contrasenya'])."'";
 
-echo $_POST['contrasenya'];
 
 // insertar usuario
 
@@ -18,13 +17,15 @@ echo $_POST['contrasenya'];
 
 
 	$query = "INSERT INTO Usuaris (Nom_Usuari, Cognom1, Cognom2, Direccio, CP, Telefon, Mobil, Password, Poblacio, Data_Naixement, email) VALUES ('".$_POST['nom']."','".$_POST['cognom1']."','".$_POST['cognom2']."','". $_POST['adrecapostal']."',".$_POST['codipostal'].",".$_POST['telefon'].",". $_POST['mobil']."," . $_POST['contrasenya'].",'". $_POST['poblacio']."','". $_POST['datadenaixement']."','". $_POST['email']."')";
-	echo $query;
 	
 
 	
 	//sql pdo prepare
 
-	sql 
+	
+	$sql = $pdo->prepare($query);
+
+	$sql->execute(); 
 
 	//sql execute
 
