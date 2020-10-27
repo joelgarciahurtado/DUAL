0e7ec4047334afa95026f8d9deabf3b7c47bdcb3
joelgarciahurtado <joelgarciah@inslapineda.cat>
2020-10-27 14:18:54 +0100

    actualización projecto 2 dual

--- projecte2DUAL/php/conexion.php
+++ projecte2DUAL/php/conexion.php
@@ -7,6 +7,7 @@ try{
 	$pw ="root";
 	$pdo = new PDO("mysql:host=$hostname;dbname=reserva_de_entrades", $username, $pw);
 
+
 } catch(PDOexception $e){
 	echo "error " . $e->getMessage() . "\n";
 	exit; 
